4bfef1102a52029f386e6c03b723de03ec158afa
Michiel Janssen <mjanssen@dat.nl>
2026-08-24 15:41:56 +0200

    AER-4118 Normalize copyright
    - Updated copyright headers
    - Add contributors (only NL) to main pom.xml

--- tomcat-standalone/src/main/java/nl/aerius/standalone/TomcatStartup.java
+++ tomcat-standalone/src/main/java/nl/aerius/standalone/TomcatStartup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright the State of the Netherlands
+ * Copyright (c) Contributors to the project
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
